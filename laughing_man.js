86682199c598da0e0a7dfd1f61da81f8ad14bc51
Joe Sullivan <jasoccer28@outlook.com>
2026-04-25 18:56:38 -0400

    finalized laughing_man.js for submission

--- laughing_man.js
+++ laughing_man.js
@@ -1,80 +1,46 @@
-const SQUARE_COUNT = 3;
-const TIMER_SPEED = 500;
-
-let box;
+let squareArea = document.querySelector("#squarearea");
+//let squareArea = document.getElementById("squarearea");
+let squareCount = parseInt(Math.random()*21)+30; //30-50
+//Make some variable for zindex
 let zIndexCounter = 1;
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector("#square").addEventListener("click", () => {
-        alert("OMG YOU CLICKED ME!");
-        squareClick(document.querySelector("#square"));
-    });
+window.onload = function() {
+    squareArea = document.querySelector("#squarearea");
 
-    box = document.querySelector("#box");
-
-    document.querySelector("#addSquareButton").addEventListener("click", addSquare);
-    document.querySelector("#changeColorsButton").addEventListener("click", changeColors);
-
-    for (let i = 0; i < SQUARE_COUNT; i++) {
+    for(let i =0; i < squareCount; i++){
         addSquare();
     }
-});
+};
 
-function addSquare(){
-    // make the element, but its not anything or on the page
-    let square = document.createElement('div');
-
-    // Set the attributes /properties of that element
+function addSquare(){ //function to add another square
+    let square = document.createElement("div");
     square.className = "square";
 
-    let size = parseInt(Math.random() * 11) + 45;
+    let size = parseInt(Math.random()*11)+45;
     square.style.width = size + "px";
     square.style.height = size + "px";
 
+    square.style.left = parseInt(Math.random()*(squareArea.clientWidth - size))+"px";
+    square.style.top = parseInt(Math.random()*(squareArea.clientHeight - size))+"px";
+
     square.style.backgroundColor = getRandomColor();
 
-    square.style.left = parseInt(Math.random() * 450) + "px";
-    square.style.top = parseInt(Math.random() * 350) + "px";
-
+    //I can put a onclick and a function(this)
     square.onclick = function(){
-        squareClick(square);
+        if(square.style.zIndex == zIndexCounter){
+            square.remove();
+        }else {
+            zIndexCounter++;
+            square.style.zIndex = zIndexCounter;
+        }
     };
 
-    box.appendChild(square);
-
-    moveSquare(square);
-}
-
-function moveSquare(element){
-    const parent = element.parentElement;
-    const maxX = parent.clientWidth - element.clientWidth;
-    const maxY = parent.clientHeight - element.clientHeight;
-
-    let dx = TIMER_SPEED * Math.random() * 2 - 1; // This gives a # from -1 to .99999
-    let dy = TIMER_SPEED * Math.random() * 2 - 1;
-
-    let x = parseInt(element.style.left) || 225;
-    let y = parseInt(element.style.top) || 175;
-
-    setInterval(() => {
-
-        if(x <=0 || x>=maxX){
-            dx*=-1;
-        }
-        if(y <=0 || y >=maxY){
-            dy *= -1;
-        }
-
-        x += dx;
-        y += dy;
-
-        element.style.left = x + "px";
-        element.style.top = y + "px";
-    }, TIMER_SPEED);
+    //put it on the screen
+    squareArea.append(square);
 }
 
 function getRandomColor(){
-    //Make a Color
+    //Make a Color ->
     let letters = "0123456789ABCDEF";
     let color = "#";
 
@@ -85,19 +51,10 @@ function getRandomColor(){
     return color; //return that string
 }
 
-function squareClick(square){
-    if(parseInt(square.style.zIndex) == zIndexCounter){
-        square.remove();
-    } else {
-        zIndexCounter++;
-        square.style.zIndex = zIndexCounter;
-    }
-}
+function changeColor(){ //function to change color of squares
+    let allSquares = document.querySelectorAll(".square");
 
-function changeColors(){
-    let allSquares = document.querySelectorAll(".square, #square");
-
-    for(let i = 0; i < allSquares.length; i++){
+    for(let i = 0; i < allSquares.length; i++){ //for loop to change color of squares
         allSquares[i].style.backgroundColor = getRandomColor();
     }
 }
